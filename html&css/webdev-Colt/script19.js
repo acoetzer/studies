ef8d66ebacaae1b035615589f7f75e3d3ed6c2f7
acoetzer <coetzeronline@gmail.com>
2022-04-13 16:18:02 +0300

    practise some js for loops

--- html&css/webdev-Colt/script19.js
+++ html&css/webdev-Colt/script19.js
@@ -18,3 +18,35 @@
 //     console.log(`Original was ${people[i]}.`)
 //     console.log(`indexOf(${i}) is - ${people[i].toUpperCase()}`)
 // }
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(`outterLoop: ${i}`)
+//     for (let j = 1; j <=5; j++) {
+//         console.log(`        innerLoop: ${j}`)
+//     }
+// }
+
+// const plants = [
+//     ['Apple', 'Plum', 'Apricot'],
+//     ['Basil', 'Thyme', 'Rosemary'],
+//     ['Tomato', 'Kale', 'Zuccini']
+// ];
+
+// for (let i = 0; i < plants.length; i++) {
+//     const type = plants[i];
+//     console.log(`type of plants ${i + 1}`)
+//     for (let j = 0; j < type.length; j++) {
+//         console.log(`    ${type[j]}`)
+//     }
+
+// }
+
+const people = ['Andre', 'Vanya', 'Dary'];
+
+// for (let i = 0; i < people.length; i++) {
+//     console.log(people[i])
+// }
+
+for (let i = people.length - 1; i >= 0; i--) {
+    console.log(people[i])
+}
