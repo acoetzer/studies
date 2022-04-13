3b122d4c697d55abff5987b33d13ee4bda11d51e
acoetzer <coetzeronline@gmail.com>
2022-04-13 14:29:04 +0300

    practise some js for loops

---
+++ html&css/webdev-Colt/script19.js
@@ -0,0 +1,20 @@
+// const userInput = prompt('Enter a number');
+
+// for (let i = 1; i <= userInput; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i} isEven`)
+//     } else {
+//         console.log(`${i} isODD`)
+//     }
+// }
+
+// for (let i = 25; i >= 0; i -= 5) {
+//     console.log(i)
+// }
+
+// const people = ['Vanya', 'Andre', 'Penka', 'Ganka', 'Stanka', 'Maria'];
+
+// for (let i = 0; i < people.length; i++) {
+//     console.log(`Original was ${people[i]}.`)
+//     console.log(`indexOf(${i}) is - ${people[i].toUpperCase()}`)
+// }
