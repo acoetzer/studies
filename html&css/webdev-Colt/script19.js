4713edb8dc2d37993e851bb9913f129d7087d9dc
acoetzer <coetzeronline@gmail.com>
2022-04-14 10:28:47 +0300

    practise some js for loops

--- html&css/webdev-Colt/script19.js
+++ html&css/webdev-Colt/script19.js
@@ -41,12 +41,36 @@
 
 // }
 
-const people = ['Andre', 'Vanya', 'Dary'];
+// const people = ['Andre', 'Vanya', 'Dary'];
 
-// for (let i = 0; i < people.length; i++) {
+// // for (let i = 0; i < people.length; i++) {
+// //     console.log(people[i])
+// // }
+
+// for (let i = people.length - 1; i >= 0; i--) {
 //     console.log(people[i])
 // }
 
-for (let i = people.length - 1; i >= 0; i--) {
-    console.log(people[i])
-}
+// Nested arrays and loops
+// let persons = [
+//     ['Andre', '32', 'Male'],
+//     ['Vanya', '57', 'Female'],
+//     ['Dary', '28', 'Female']
+// ]
+
+// for (let i = 0; i < persons.length; i++) {
+//     const individual = persons[i];
+//     console.log(`At indexOf ${i} is:`)
+//     for (let j = 0; j < individual.length; j++){
+//         console.log(`          ${individual[j]}`)
+//     }
+// }
+
+// Single array
+
+// const colors = ['lime', 'lightblue', 'yellow'];
+
+// for (let i = colors.length - 1; i >= 0; i--) {
+//     console.log(`${i + 1} ${colors[i]}`)
+// }
+
