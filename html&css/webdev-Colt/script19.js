79822b15f50caee579a4c713455163f2c64068ec
acoetzer <coetzeronline@gmail.com>
2022-04-22 17:27:22 +0300

    practicing JavaScript for loops & while loops

--- html&css/webdev-Colt/script19.js
+++ html&css/webdev-Colt/script19.js
@@ -1,283 +1,213 @@
-// const userInput = prompt('Enter a number');
+// FOR LOOPS PRACTICE
+// *****************
 
-// for (let i = 1; i <= userInput; i++) {
-//     if (i % 2 === 0) {
-//         console.log(`${i} isEven`)
-//     } else {
-//         console.log(`${i} isODD`)
-//     }
-// }
+console.log('--- Basic For Loop ---');
+for (let i = 1; i <= 5; i++) {
+    console.log(`${i} iteration`);
+}
+console.log('--- END ---');
 
-// for (let i = 25; i >= 0; i -= 5) {
-//     console.log(i)
-// }
+console.log(' ');
+console.log('--- Basic For Loop in Reverse ---');
+for (let i = 5; i >= 1; i -= 1) {
+    console.log(`${i} iteration`);
+}
+console.log('--- END ---');
 
-// const people = ['Vanya', 'Andre', 'Penka', 'Ganka', 'Stanka', 'Maria'];
+// BASIC FOR LOOP ODDS & EVEN COUNT PRACTICE
+// *****************
 
-// for (let i = 0; i < people.length; i++) {
-//     console.log(`Original was ${people[i]}.`)
-//     console.log(`indexOf(${i}) is - ${people[i].toUpperCase()}`)
-// }
+console.log(' ');
+console.log('--- Basic For Loop Even Count ---');
+for (let i = 2; i <= 10; i += 2) {
+    console.log(`EVEN: ${i}`)
+}
+console.log('--- END ---');
 
-// for (let i = 0; i <= 10; i++){
-//     console.log(`outterLoop: ${i}`)
-//     for (let j = 1; j <=5; j++) {
-//         console.log(`        innerLoop: ${j}`)
-//     }
-// }
+console.log(' ');
+console.log('--- Basic For Loop Odds Count ---');
+for (let i = 1; i <= 10; i += 2) {
+    console.log(`EVEN: ${i}`)
+}
+console.log('--- END ---');
 
-// const plants = [
-//     ['Apple', 'Plum', 'Apricot'],
-//     ['Basil', 'Thyme', 'Rosemary'],
-//     ['Tomato', 'Kale', 'Zuccini']
-// ];
+// FOR LOOPS OVER AN ARRAY PRACTICE
+// *****************
 
-// for (let i = 0; i < plants.length; i++) {
-//     const type = plants[i];
-//     console.log(`type of plants ${i + 1}`)
-//     for (let j = 0; j < type.length; j++) {
-//         console.log(`    ${type[j]}`)
-//     }
+console.log(' ');
+console.log('--- For Loop Iterating Over an Array ---');
 
-// }
+const students = ['Colt', 'Brian', 'Jerry', 'Sam'];
 
-// const people = ['Andre', 'Vanya', 'Dary'];
+for (let i = 0; i < students.length; i++) {
+    console.log(`Present students are: ${students[i]}`)
+}
+console.log('--- END ---');
 
-// // for (let i = 0; i < people.length; i++) {
-// //     console.log(people[i])
-// // }
+console.log(' ');
+console.log('--- For Loop Iterating Over an Array in Reverse ---');
 
-// for (let i = people.length - 1; i >= 0; i--) {
-//     console.log(people[i])
-// }
+for (let i = students.length - 1; i >= 0; i--) {
+    console.log(`Students starting from the back are: ${students[i]}`)
+}
+console.log('--- END ---');
 
-// Nested arrays and loops
-// let persons = [
-//     ['Andre', '32', 'Male'],
-//     ['Vanya', '57', 'Female'],
-//     ['Dary', '28', 'Female']
-// ]
+// NESTED FOR LOOPS PRACTICE
+// *****************
 
-// for (let i = 0; i < persons.length; i++) {
-//     const individual = persons[i];
-//     console.log(`At indexOf ${i} is:`)
-//     for (let j = 0; j < individual.length; j++){
-//         console.log(`          ${individual[j]}`)
-//     }
-// }
+console.log(' ');
+console.log('--- Basic Nested For Loop ---');
 
-// Single array
+for (let i = 1; i <= 3; i++) {
+    console.log(`Outer: ${i}`)
+    for (let j = 1; j <= 2; j++) {
+        console.log(`   Inner: ${i}.${j}`)
+    }
+}
+console.log('--- END ---');
 
-// const colors = ['lime', 'lightblue', 'yellow'];
+console.log(' ');
+console.log('--- Basic Nested For Loop in Reverse ---');
 
-// for (let i = colors.length - 1; i >= 0; i--) {
-//     console.log(`${i + 1} ${colors[i]}`)
-// }
+for (let i = 3; i >= 1; i--) {
+    console.log(`outer: ${i}`)
+    for (let j = 2; j >= 1; j--) {
+        console.log(`   Inner: ${i}.${j}`)
+    }
+}
+console.log('--- END ---');
 
-// for (let i = 1; i <= 5; i++) {
-//     console.log(`The outter loop is :${i}`)
-// }
+// NESTED FOR LOOPS OVER NESTED ARRAYS PRACTICE
+// *****************
 
-// for (let i = 1; i <= 3; i++) {
-//     console.log(`The Outter loop is :${i}`)
-//     for (let j = 1; j <= 3; j += 1) {
-//         console.log(`    The Inner loop is : ${j}`)
-//     }
-// }
+console.log(' ');
+console.log('--- Basic Nested For Loop over Nested Arrays ---');
 
-// const people = ['Andre', 'Vanya', 'Dary'];
+const classroom = [
+    ['Colt', 'Brain', 'Sam'],
+    ['Andre', 'Vanya', 'Dary'],
+    ['Tom', 'Jerry', 'Scooby']
+]
 
-// for (let i = 0; i < people.length; i++) {
-//     console.log(`At index ${i} is ${people[i]}`)
-// }
-// console.log(`There are ${people.length} persons here.`)
+for (let i = 0; i < classroom.length; i++) {
+    const row = classroom[i];
+    console.log(`Row ${i + 1} students are:`)
+    for (let j = 0; j < row.length; j++) {
+        console.log(`   ${i + 1}.${j + 1} ${row[j]}`)
+    }
+}
+console.log('--- END ---');
 
-// const people = [
-//     ['Andre', '32'],
-//     ['Vanya', '33'],
-//     ['Dary', '28']
-// ]
+console.log(' ');
+console.log('--- Basic Nested For Loop over Nested Arrays in Reverse ---');
 
-// for (let i = 0; i < people.length; i++) {
-//     const row = people[i];
-//     console.log(`At index ${i}, we have:`)
-//     for (let j = 0; j < row.length; j++) {
-//         console.log(`${row}`)
-//     }
-// }
+for (let i = classroom.length - 1; i >= 0; i--) {
+    const row = classroom[i];
+    console.log(`Row ${i + 1} students are:`)
+    for (let j = row.length - 1; j >= 0; j--) {
+        console.log(`   ${i + 1}.${j + 1} ${row[j]}`)
+    }
+}
+console.log('--- END ---');
 
-// const places = [
-//     ['Canada', 'Northern Americas'],
-//     ['Bulgaria', 'Eastern Europe'],
-//     ['Republic of South Africa', 'Southern Africa'],
-// ];
+// FOR LOOP TO DETERMINE EVEN || ODD FROM USER INPUT
+// *****************
 
-// for (let i = 0; i < places.length; i++) {
-//     const row = places[i];
-//     console.log(`At indexOf ${i} is:`)
-//     for (let j = 0; j < row.length; j++) {
-//         console.log(`    ${row[j]}`)
-//     }
-// }
+console.log(' ');
+console.log('--- For Loop to Determine Even || Odd from User Input ---');
 
-// for (let i = 0; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(`${i} is Even`)
-//     } else {
-//         console.log(`${i} is Odd`)
-//     }
-// }
+let userNum = parseInt(prompt(`Pick a number for Even & Odds iteration`));
+while (!userNum) {
+    alert('DO NOT ENTER TEXT');
+    userNum = parseInt(prompt(`Pick a number for Even & Odds iteration`));
+}
+alert('Check Console for Iteration Outcome')
 
-// for (let i = 0; i <= 3; i++) {
-//     console.log(`The outter loop of :${i}`);
-//     for (j = 1; j <= 2; j++) {
-//         console.log(`       The inner loop index is :${j}`)
-//     }
-// }
+for (i = 0; i <= userNum; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is Even`);
+    } else {
+        console.log(`${i} is Odd`);
+    }
+}
+console.log('--- END ---');
 
-// const people = [
-//     ['Andre', 'Vanya', 'Dary'],
-//     ['Mom', 'Dad'],
-//     ['Maureen', 'Mark', 'Alicia']
-// ];
+// BASIC WHILE LOOP
+// *****************
 
-// for (let i = 0; i < people.length; i++) {
-//     // const row = people[i];
-//     console.log(i);
-//     for (let j = 0; j < people[i].length; j++ ) {
-//         // console.log(`   ${row[j]}`);
-//         console.log(`   ${j}`);
-//     }
-// }
+console.log(' ');
+console.log('--- Basic While Loop ---');
 
-// let userInput = prompt('Say Something"')
+let count = 1;
 
-// while (true) {
-//     if (userInput.toLowerCase() === 'stop copying me') {
-//         break;
-//     } else {
-//         userInput = prompt(userInput);
-//     }
-// }
-// alert('Sure thing!')
+while (count <= 5) {
+    console.log(`${count} iteration`);
+    count++;
+}
+console.log('--- END ---');
 
-// let choice = parseInt(prompt('Enter a maximun number for the game.'));
-// while (!choice) {
-//     choice = parseInt(prompt('Enter a Number, not Text!'));
-// }
-// const targetNum = Math.round(Math.random() * choice);
+console.log(' ');
+console.log('--- Basic While Loop in Reverse ---');
 
-// let guess = parseInt(prompt('Enter your first guess?'));
-// let attemps = 1;
-// const history = [];
-// while (parseInt(guess) !== targetNum) {
-//     history.push(guess)
-//     if (guess === 'q') {
-//         break;
-//     } 
-//     attemps++;
-//     if (guess > targetNum) {
-//         guess = prompt('Too high, guess again');
-//     }   else {
-//         guess = prompt('Too low, guess again');
-//     }
-// }
+let countRev = 5;
 
-// if (guess === 'q') {
-//     alert('Ok you quit');
-// } else {
-//     alert(`Congratulations`);
-//     alert(`The hidden number was ${targetNum} of ${choice} and your guess was ${guess} on ${attemps} attemps `);
-//     alert(`Your guesses were ${history} and the final ${guess}`)
-// }
+while (countRev >= 1) {
+    console.log(`${countRev} iteration`);
+    countRev--;
+}
+console.log('--- END ---');
 
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+// BASIC WHILE LOOP GUESS CODE
+// *****************
 
-// // WRITE YOUR LOOP BELOW THIS LINE:
-// for (num of numbers) {
-//     console.log(num * num)
-// }
+console.log(' ');
+console.log('--- Basic While Loop Code Guess ---');
 
-// let num = 1;
+const secretCode = 'let me in';
 
-// while (num <= 5) {
-//     console.log(num)
-//     num++;
-// }
+let userInput = prompt('Enter the secret code...');
+while (userInput !== secretCode) {
+    userInput = prompt('Enter the secret code...');
+}
+console.log('--- END ---');
 
-// for (let i = 1; i <= 5; i++) {
-//     console.log(i)
-// }
+// WHILE LOOP GUESSING GAME
+// *****************
 
-// let userInput = prompt('Say something').toLowerCase();
+console.log(' ');
+console.log('--- Guessing Game ---');
 
-// while (true) {
-//     if (userInput === 'stop') {
-//         break;
-//     } else {
-//         userInput = prompt(userInput);
-//     }
-// }
+let userChoice = parseInt(prompt('Pick a maximum number for a guessing game...'));
+while (!userChoice) {
+    alert('DO NOT INPUT TEXT!')
+    userChoice = parseInt(prompt('Pick a Maximum number for a guessing game...'));
+}
 
-// const classroom = [
-//     ['Andre', 'Vanya', 'Dary'],
-//     ['Mom', 'Dad'],
-//     ['Buddy', 'Diesel', 'Goofy']
-// ]
+let randomNum = Math.round(Math.random() * userChoice);
 
-// for (let i = 0; i < classroom.length; i++) {
-//     const row = classroom[i];
-//     console.log(i);
-//     for (let j = 0; j < row.length; j++) {
-//         console.log(`   ${row[j]}`);
-//     }
-// }
+let userGuess = parseInt(prompt(`Take a guess between 0 and ${userChoice}`));
+while (!userGuess) {
+    alert('DO NOT INPUT TEXT!')
+    userGuess = parseInt(prompt(`Take a guess between 0 and ${userChoice}`));
+}
 
-// let num = 0;
+let scoreKeeper = 1;
+const tries = [];
 
-// for (let row of classroom) {
-//     console.log(num)
-//     num++;
-//     for (let student of row) {
-//         console.log(`   ${student}`)
-//     }
-// }
+while (userGuess !== randomNum) {
+    scoreKeeper++;
+    tries.push(userGuess)
+    if (userGuess > randomNum) {
+        userGuess = parseInt(prompt('Too High, Guess again...'));
+    } else {
+        userGuess = parseInt(prompt('Too Low, Guess again...'));
+    }
+}
+alert(`Correct, The Random Number was ${randomNum} and your guess was ${userGuess}.`)
+alert(`It took you, ${scoreKeeper} times before guessing correctly, and your previous guesses were ${tries}.`)
 
-// for (let char of 'Vanya') {
-//     console.log(char)
-// }
-
-// const myname = 'Andre'
-// for (let i = 0; i < myname.length; i++) {
-//     console.log(myname[i])
-// }
-
-// let input = prompt('Enter a Name');
-
-// let userName = '';
-
-// // for (let i = 0; i < input.length; i++) {
-// //     if (i % 2 === 0) {
-// //         userName = userName + input[i].toUpperCase();
-// //     } else {
-// //         userName = userName + input[i].toLowerCase()
-// //     }
-// // }
-// alert(userName)
-
-// const classroom = ['Andre', 'Vanya', 'Dary', 'Colt', 'Keele'];
-
-// for (let i = 0; i < classroom.length; i++) {
-//     console.log(classroom[i])
-// }
-
-// const classroom = {
-//     Andre: 'Present',
-//     Vanya: 'Absent',
-//     Dary: 'Sleeping',
-//     Buddy: 'Playing'
-// }
-
-// for (let student in classroom) {
-//     console.log(`${student} is ${classroom[student]}`)
-// }
+console.log(`User Choice was: ${userChoice}`)
+console.log(`Random Number was: ${randomNum}`)
+console.log(`User Guess was: ${userGuess} on ${scoreKeeper} tries`)
+console.log(`User's Guess history is: ${tries}`)
+console.log('--- END ---');
