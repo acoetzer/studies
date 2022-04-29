e7b82de130b94ed95d130b84bdeb0c9f754744ae
acoetzer <coetzeronline@gmail.com>
2022-04-29 18:30:26 +0300

    added some practise functions

--- html&css/webdev-Colt/script21.js
+++ html&css/webdev-Colt/script21.js
@@ -29,3 +29,74 @@
 //     }
 //     callForHelp();
 // }
+
+// function callTwice (func) {
+//     func();
+//     func();
+// }
+
+// function laugh () {
+//     console.log('Ha Ha')
+// }
+
+// callTwice(laugh);
+
+// function callTenTimes (f) {
+//     for (let i = 1; i <= 10; i++) {
+//         f()
+//     }
+// }
+
+// function random () {
+//     const randNum = Math.round(Math.random() * 10)
+//     console.log(randNum)
+// }
+
+// callTenTimes(random)
+
+// function makeMysteryFunc() {
+//     const randomNum = Math.round(Math.random() * 10);
+//     if (randomNum > 5) {
+//         return function () {
+//             console.log('This is a Happy Function')
+//             console.log('You understand how to write this shit')
+//         }
+//     } else {
+//         return function () {
+//             console.log('This is a Sad Function')
+//             console.log('But at least you understand this shit!')
+//         }
+//     }
+// }
+
+// function makeBetweenAge (min, max) {
+//     return function (num) {
+//         return num >= min && num <= max;
+//     }
+// }
+
+// const myMath = {
+//         PI: 3.14,
+//         add: function (x, y) {
+//             return x + y;
+//         },
+//         multiply(x, y) {
+//             return x * y;
+//         },
+//         divide(x, y) {
+//             return x / y;
+//         }
+// }
+
+const wife = {
+    name: 'Vanya',
+    age: 33,
+    favWords1: 'Go to Scott',
+    favWords2: 'Stiga ce yel!',
+    saying1: function () {
+        return `${this.name} says ${this.favWords1}`
+    },
+    saying2: function () {
+        return `${this.name} says ${this.favWords2}`
+    }
+}
