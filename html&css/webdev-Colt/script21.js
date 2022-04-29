2f7e165bc531dcb80e4ce591042b31ea77746a22
acoetzer <coetzeronline@gmail.com>
2022-04-29 13:48:55 +0300

    add scope info and function expression info

---
+++ html&css/webdev-Colt/script21.js
@@ -0,0 +1,31 @@
+// let forecast = 'rainy day';
+
+// function weather () {
+//     let forecast = 'sunny weather';
+//     console.log(forecast);
+// }
+// weather();
+// console.log(forecast);
+
+
+// let sky = 'blue';
+
+// if (sky === 'blue') {
+//     let pi = Math.PI;
+//     let color = 'light Blue';
+// }
+
+// console.log(pi);
+// console.log(color);
+// console.log(sky);
+
+
+// function bankRobbery () { 
+//     const heroes = ['Spiderman', 'Batman', 'Superman', 'Daredevil', 'Dead Pool']; 
+//     function callForHelp () { 
+//         for (let i = 0; i < heroes.length; i++) {
+//             console.log(`Help Me ${heroes[i]}`)
+//         }
+//     }
+//     callForHelp();
+// }
