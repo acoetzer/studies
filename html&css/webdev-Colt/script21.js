19aa4c4f7a883e69612088f05aa5fd36cae0cdbc
acoetzer <coetzeronline@gmail.com>
2022-04-30 13:17:32 +0300

    Few things done, however finish section 21
    In the .css added a header h2 selector
    in section 21, finish section
    in .js practise some functions and while loops

--- html&css/webdev-Colt/script21.js
+++ html&css/webdev-Colt/script21.js
@@ -88,15 +88,89 @@
 //         }
 // }
 
-const wife = {
-    name: 'Vanya',
-    age: 33,
-    favWords1: 'Go to Scott',
-    favWords2: 'Stiga ce yel!',
-    saying1: function () {
-        return `${this.name} says ${this.favWords1}`
-    },
-    saying2: function () {
-        return `${this.name} says ${this.favWords2}`
+// const wife = {
+//     name: 'Vanya',
+//     age: 33,
+//     favWords1: 'Go to Scott',
+//     favWords2: 'Stiga ce yel!',
+//     saying1: function () {
+//         return `${this.name} says ${this.favWords1}`
+//     },
+//     saying2: function () {
+//         return `${this.name} says ${this.favWords2}`
+//     }
+// }
+
+function makeRandomFunc () {
+    let randomNum = Math.round(Math.random() * 10);
+    if (randomNum > 5) {
+        return function () {
+            return 'This is a Happy Function';
+        }
+    } else {
+        return function () {
+            return 'This is a Sad Function';
+        }
     }
 }
+
+function makeBetweenFunc (min, max) {
+    return function (num) {
+        return num >= min && num <= max;
+    }
+}
+
+// function duplicateMe (f) {
+//     for (let i = 0; i < 10; i++) {
+//         f();
+//     }
+// }
+
+// function randomNumber () {
+//     console.log(Math.round(Math.random() * 10));
+// }
+
+// duplicateMe(randomNumber);
+
+
+const cucumber = {
+    name: 'Cucumis sativus',
+    type: 'Creeping Vine',
+    says: 'Grow MEEEEE!',
+    length: 0,
+    grow() {
+        this.length += 10;
+        return `${this.name} has grown by 10cm.`;
+    }
+}
+
+// let userInput = prompt('what would you like to do?').toLowerCase();
+
+// const list = [];
+
+// while (userInput !== 'quit' && userInput !== 'q') {
+//     if (userInput === 'new') {
+//         let newInput = prompt("What's your new ToDo?");
+//         list.push(newInput);
+//         alert(`${newInput} was added to the ToDo list.`);
+//     } else if (userInput === 'list') {
+//         console.log('**********');
+//         for (let i = 0; i < list.length; i++) {
+//             console.log(`${i + 1}. ${list[i]}`);
+//         }
+//         console.log('**********');
+//     } else if (userInput === 'delete') {
+//         console.log('*****!DELETE!*****');
+//         for (let i = 0; i < list.length; i++) {
+//             console.log(`${i + 1}. ${list[i]}`);
+//         }
+//         console.log('*****!WARNING!*****');
+//         let deleteInput = prompt('Which Number Would You Like To Delete?')
+//         let deleted = list.splice(deleteInput - 1, 1);
+//         alert(`${deleted} was removed from the list`)
+//         console.log('*****!REMOVED!*****');
+//         console.log(`${deleted} was removed from the list`);
+//         console.log('*****!REMOVED!*****');
+//     }
+//     userInput = prompt('what would you like to do?').toLowerCase();
+// }
