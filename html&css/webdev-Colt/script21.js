8fa0a2e0ae703f9a3ed42920f774209d4da5877a
acoetzer <coetzeronline@gmail.com>
2022-04-30 20:50:55 +0300

    Practise functions and made a function game

--- html&css/webdev-Colt/script21.js
+++ html&css/webdev-Colt/script21.js
@@ -1,176 +1,175 @@
-// let forecast = 'rainy day';
+// Function Scopes
+// *****************
 
-// function weather () {
-//     let forecast = 'sunny weather';
-//     console.log(forecast);
-// }
-// weather();
-// console.log(forecast);
+function rainbow () {
+    const colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+    for (color of colors) {
+        console.log(color)
+    }
+}
+rainbow()
 
-
-// let sky = 'blue';
-
-// if (sky === 'blue') {
-//     let pi = Math.PI;
-//     let color = 'light Blue';
-// }
-
-// console.log(pi);
-// console.log(color);
-// console.log(sky);
+try {
+    console.log(colors)
+} catch {
+    console.log('Error!')
+    console.log('You cannot reach a variable scoped to a function')
+}
 
 
-// function bankRobbery () { 
-//     const heroes = ['Spiderman', 'Batman', 'Superman', 'Daredevil', 'Dead Pool']; 
-//     function callForHelp () { 
-//         for (let i = 0; i < heroes.length; i++) {
-//             console.log(`Help Me ${heroes[i]}`)
-//         }
-//     }
-//     callForHelp();
-// }
+// Block Scopes
+// *****************
+console.log('')
 
-// function callTwice (func) {
-//     func();
-//     func();
-// }
+const blockScopeNum = 5;
 
-// function laugh () {
-//     console.log('Ha Ha')
-// }
+if (blockScopeNum > 0) {
+    let blockColor =  'Red';
+    console.log(blockColor)
+}
 
-// callTwice(laugh);
+try {
+    console.log(blockColor);
+} catch {
+    console.log('Error!')
+    console.log('You cannot reach a variable scoped to a block of code')
+}
 
-// function callTenTimes (f) {
-//     for (let i = 1; i <= 10; i++) {
-//         f()
-//     }
-// }
+// Lexical Scopes
+// *****************
+console.log('')
 
-// function random () {
-//     const randNum = Math.round(Math.random() * 10)
-//     console.log(randNum)
-// }
+function plants () {
+    const plantList = ['Basil', 'Cucumber', 'Zuccini', 'Avocado', 'Chili', 'Kale'];
+    function myPlants (){
+        for (let i = 0; i < plantList.length; i++) {
+            console.log(`We have ${plantList[i]}`)
+        }
+    }
+    myPlants();
+}
+plants();
 
-// callTenTimes(random)
+try {
+    console.log(plantList);
+} catch {
+    console.log('Error!')
+    console.log('You cannot call a lexical scoped varaible')
+}
 
-// function makeMysteryFunc() {
-//     const randomNum = Math.round(Math.random() * 10);
-//     if (randomNum > 5) {
-//         return function () {
-//             console.log('This is a Happy Function')
-//             console.log('You understand how to write this shit')
-//         }
-//     } else {
-//         return function () {
-//             console.log('This is a Sad Function')
-//             console.log('But at least you understand this shit!')
-//         }
-//     }
-// }
+// Function Expressions
+// *****************
+console.log('')
 
-// function makeBetweenAge (min, max) {
-//     return function (num) {
-//         return num >= min && num <= max;
-//     }
-// }
+const collectEggs = function () {
+    return 'EGG'
+}
 
-// const myMath = {
-//         PI: 3.14,
-//         add: function (x, y) {
-//             return x + y;
-//         },
-//         multiply(x, y) {
-//             return x * y;
-//         },
-//         divide(x, y) {
-//             return x / y;
-//         }
-// }
+// Higher Order Functions - Functions as Arguments
+// *****************
+console.log('')
 
-// const wife = {
-//     name: 'Vanya',
-//     age: 33,
-//     favWords1: 'Go to Scott',
-//     favWords2: 'Stiga ce yel!',
-//     saying1: function () {
-//         return `${this.name} says ${this.favWords1}`
-//     },
-//     saying2: function () {
-//         return `${this.name} says ${this.favWords2}`
-//     }
-// }
+function dice (func) {
+    func();
+    func();
+}
 
-function makeRandomFunc () {
-    let randomNum = Math.round(Math.random() * 10);
-    if (randomNum > 5) {
+function singleDie () {
+    console.log(Math.round(Math.random() * 6));
+}
+
+dice(singleDie);
+
+// Higher Order Functions - Returning Functions
+// *****************
+console.log('')
+
+// Return a random function
+function giveMeARandomFunc () {
+    const randomGen = Math.round(Math.random() * 2)
+    if (randomGen === 0) {
         return function () {
-            return 'This is a Happy Function';
+            console.log('This is a Happy Function');
+        }
+    } else if (randomGen === 1) {
+        return function () {
+            console.log('You found Spice, becareful of the worm!')
         }
     } else {
         return function () {
-            return 'This is a Sad Function';
+            console.log('This is desert power!')
         }
     }
 }
 
-function makeBetweenFunc (min, max) {
+// Factory Function
+function factoryFunction (min, max) {
     return function (num) {
         return num >= min && num <= max;
     }
 }
 
-// function duplicateMe (f) {
-//     for (let i = 0; i < 10; i++) {
-//         f();
-//     }
-// }
+// Defining Methods
+// *****************
+console.log('')
 
-// function randomNumber () {
-//     console.log(Math.round(Math.random() * 10));
-// }
-
-// duplicateMe(randomNumber);
-
-
-const cucumber = {
-    name: 'Cucumis sativus',
-    type: 'Creeping Vine',
-    says: 'Grow MEEEEE!',
-    length: 0,
-    grow() {
-        this.length += 10;
-        return `${this.name} has grown by 10cm.`;
+const bunny1 = {
+    name: 'Harry the Bunny',
+    breed: 'Siberian',
+    says() {
+        return 'Feed me, I am hungry!';
     }
 }
 
-// let userInput = prompt('what would you like to do?').toLowerCase();
+// The "this" Keyword
+// *****************
 
-// const list = [];
+console.log('myBunny.')
+console.log('name, breed, says(), health, searchForFood(), sleep')
 
-// while (userInput !== 'quit' && userInput !== 'q') {
-//     if (userInput === 'new') {
-//         let newInput = prompt("What's your new ToDo?");
-//         list.push(newInput);
-//         alert(`${newInput} was added to the ToDo list.`);
-//     } else if (userInput === 'list') {
-//         console.log('**********');
-//         for (let i = 0; i < list.length; i++) {
-//             console.log(`${i + 1}. ${list[i]}`);
-//         }
-//         console.log('**********');
-//     } else if (userInput === 'delete') {
-//         console.log('*****!DELETE!*****');
-//         for (let i = 0; i < list.length; i++) {
-//             console.log(`${i + 1}. ${list[i]}`);
-//         }
-//         console.log('*****!WARNING!*****');
-//         let deleteInput = prompt('Which Number Would You Like To Delete?')
-//         let deleted = list.splice(deleteInput - 1, 1);
-//         alert(`${deleted} was removed from the list`)
-//         console.log('*****!REMOVED!*****');
-//         console.log(`${deleted} was removed from the list`);
-//         console.log('*****!REMOVED!*****');
-//     }
-//     userInput = prompt('what would you like to do?').toLowerCase();
-// }
+const myBunny = {
+    name: 'Harry the Bunny',
+    breed: 'Siberian',
+    says() {
+        while (this.health < 100) {
+            return 'Feed me, I am hungry!';
+            break;
+        }
+        return 'I am full, I want to sleep now';
+    },
+    health: 10,
+    searchForFood() {
+        let random = Math.round(Math.random() * 3)
+        while (this.health < 100) {
+            if (random === 0) {
+                this.health += 5;
+                return `${this.name} has found a carrot and gained 5 health. ${this.name}'s health is now ${this.health}`;
+            } else if (random === 1) {
+                this.health -= 2;
+                return `${this.name} is being chased by a crazed hunter with an axe. ${this.name} has lost 2 health. ${this.name}'s health is now ${this.health}`;
+            } else if (random === 2) {
+                this.health += 15;
+                return `${this.name} has found a delicious cabbage and gained 15 health. ${this.name}'s health is now ${this.health}`;
+            } else if (random === 3) {
+                let scopeRandom = Math.round(Math.random() * 10)
+                if (scopeRandom > 0) {
+                    return `${this.name} is in the scopes of a hunter, the hunter missed`
+                } else {
+                    this.health = 0;
+                    return `${this.name} is in the scopes of a hunter, the hunter hit and ${this.name} is dead. Start Over. ${this.name}'s health is now ${this.health}`
+                } 
+            }
+            return `${this.name} is full, try to sleep to reset your game`
+        }
+    },
+    sleep() {
+        let sleepRandom = Math.round(Math.random() * 1);
+        if (sleepRandom > 0) {
+            this.health = 10;
+            return `${this.name} has slept well, and its health is back to ${this.health}`
+        } else {
+            this.health = 5;
+            return `${this.name} had a terrible sleep, The wind had woken him up, ${this.name}'s health is now ${this.health}`
+        }
+    }
+}
