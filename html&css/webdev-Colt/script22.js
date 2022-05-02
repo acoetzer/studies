c326509249fe3b9f5639e6c778a335a4bdc0d9cb
acoetzer <coetzeronline@gmail.com>
2022-05-02 15:35:37 +0300

    practixe arrow functions, and array callback methods

---
+++ html&css/webdev-Colt/script22.js
@@ -0,0 +1,147 @@
+const movies = [
+    {
+        title: ' The Godfather',
+        score: '9.2'
+    },
+    {
+        title: 'The Godfather: Part II',
+        score: '9.0'
+    },
+    {
+        title: '12 Angry Men',
+        score: '9.0'
+    },
+    {
+        title: "Schindler's List",
+        score: '8.5'
+    },
+    {
+        title: 'Alien',
+        score: '8.0'
+    },
+    {
+        title: 'The Elephant Man',
+        score: '8.3'
+    },
+    {
+        title: 'To Be or Not to Be',
+        score: '7.5'
+    },
+    {
+        title: 'Rush',
+        score: '6.0'
+    },
+];
+
+// const goodMovies = movies.filter(el => m.score >= 8.3).map(el => m.title);
+
+// function isEven (arr) {
+//     return arr.filter(function (num) {
+//         return num % 2 === 0;
+//     })
+// }
+
+// const isEven = (num) => {
+//     return num % 2 === 0;
+// }
+
+// const isEven = num => {
+//     return num % 2 === 0;
+// }
+
+// const isEven = num => (
+//     num % 2 === 0;
+// )
+
+// const isEven = num => num % 2 === 0;
+
+
+
+// const familyMembers = [
+//     {
+//         name: 'Andre',
+//         age: 32
+//     },
+//     {
+//         name: 'Vanya',
+//         age: 34
+//     },
+// ];
+
+// familyMembers.forEach(function (el) {
+//     console.log(el.name, el.age);
+// })
+
+// for (person of people) {
+//     console.log(person.name, person.age)
+// }
+
+// for (let i = 0; i < people.length; i++) {
+//     console.log(people[i].name, people[i].age)
+// }
+
+// const doubles = numbers.map(function (num) {
+//     return num * 2;
+// })
+
+// console.log(numbers)
+// console.log(doubles)
+
+// const familyNames = familyMembers.map(function (person) {
+//     return person.name.toUpperCase();
+// })
+
+// console.log(familyMembers)
+// console.log(familyNames)
+
+// // 
+// function isEven1 (num) {
+//     return num % 2 === 0;
+// }
+// console.log(isEven1(1))
+
+// // 
+// const isEven2 = function (num) {
+//     return num % 2 === 0;
+// }
+// console.log(isEven2(2))
+
+// // 
+// const isEven3 = (num) => {
+//     return num % 2 === 0;
+// }
+// console.log(isEven3(3))
+
+// // 
+// const isEven4 = num => {
+//     return num % 2 === 0;
+// }
+// console.log(isEven4(4))
+
+// // 
+// const randomNum = () => (
+//     Math.round(Math.random() * 10)
+// )
+// console.log(randomNum())
+
+// // 
+// const isEven5 = num => num % 2 === 0;
+// console.log(isEven5(5))
+
+// console.log('Knock Knock')
+
+// const knocking = setInterval(() => {
+//     console.log('Knock Knock');
+// }, 2000)
+
+// setTimeout(() => {
+//     clearInterval(knocking)
+// }, 6000)
+
+// setTimeout(() => {
+//     console.log('Is anyone home?');
+// }, 7000)
+
+// setTimeout(() => {
+//     console.log('KNOCK KNOCK');
+// }, 8000)
