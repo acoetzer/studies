f7109f3544604405c60f13b64056ba81d6d31a91
acoetzer <coetzeronline@gmail.com>
2022-05-06 13:30:27 +0300

    Practicing shit

---
+++ html&css/webdev-Colt/script23.js
@@ -0,0 +1,67 @@
+// Some Data
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const raceResults = ['Vanya', 'Andre', 'Jeanette', 'Johan', 'Buddy', 'Mike', 'Sam', 'Fiona', 'Peter', 'Joey'];
+
+const movieList = [
+    {
+        title: 'The Shawshank Redemption',
+        score: 9.2,
+        year: 1994
+    },
+    {
+        title: 'The Godfather',
+        score: 9.2,
+        year: 1972
+    },
+    {
+        title: 'The Dark Knight',
+        score: 9.0,
+        year: 2008
+    },
+    {
+        title: '12 Angry Men',
+        score: 8.9,
+        year: 1957
+    },
+    {
+        title: 'The Good, the Bad and the Ugly',
+        score: 8.8,
+        year: 1966
+    }
+];
+
+// Default Params
+// Old Way
+// function rollDie (numSides) {
+//     if (numSides === undefined) {
+//         numSides = 6;
+//     }
+//     return Math.floor(Math.random() * numSides) + 1;
+// }
+
+// New Way
+// function rollDie (numSides = 6) {
+//     return Math.floor(Math.random() * numSides) + 1;
+// }
+
+// Spread with Function calls
+
+
+// Spread with Arrays
+
+
+// Spread with Objects
+
+
+// Rest Params
+
+
+// Destructing Arrays
+
+
+// Destructing Objects
+
+
+// Param Destructing 
