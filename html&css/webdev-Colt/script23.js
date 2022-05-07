be9a33d16a0b0634f8c906a2462b9442c2a3cfbd
acoetzer <coetzeronline@gmail.com>
2022-05-07 10:04:15 +0300

    practice some more JavaScript expressions and syntax

--- html&css/webdev-Colt/script23.js
+++ html&css/webdev-Colt/script23.js
@@ -1,6 +1,7 @@
 // Some Data
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nums1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nums2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 const raceResults = ['Vanya', 'Andre', 'Jeanette', 'Johan', 'Buddy', 'Mike', 'Sam', 'Fiona', 'Peter', 'Joey'];
 
@@ -32,36 +33,137 @@ const movieList = [
     }
 ];
 
-// Default Params
-// Old Way
-// function rollDie (numSides) {
-//     if (numSides === undefined) {
-//         numSides = 6;
-//     }
-//     return Math.floor(Math.random() * numSides) + 1;
-// }
+const groupOne = {
+    gold: 'Taylor',
+    silver: 'Perre',
+    tie: 'Sammy'
+}
 
+const groupTwo = {
+    tie: 'Lexi',
+    fifth: 'Andrea',
+    sixth: 'Andrew'
+}
+
+
+const user = {
+    firstName: 'Jose',
+    lastName: 'Taco del Rio',
+    age: 89,
+    born: 1910,
+    gender: 'Male',
+    city: 'San Jose',
+    state: 'California'
+}
+
+
+
+// Default Params
+console.log('Default Params')
+console.log('----------')
+
+// Old Way
+function rollDie1 (numSides) {
+    if (numSides === undefined) {
+        numSides = 6;
+    }
+    return Math.floor(Math.random() * numSides) + 1;
+}
+console.log(`Old way ${rollDie1()} from default 6`)
 // New Way
-// function rollDie (numSides = 6) {
-//     return Math.floor(Math.random() * numSides) + 1;
-// }
+function rollDie2 (numSides = 12) {
+    return Math.floor(Math.random() * numSides) + 1;
+}
+console.log(`New way ${rollDie2()} from default 12`)
 
 // Spread with Function calls
+console.log('')
+console.log('Spread with Function calls')
+console.log('----------')
 
+console.log(`Spread in Math.max(...nums1): ${Math.max(...nums1)}`)
+console.log(`Spread in Math.min(...nums1): ${Math.min(...nums1)}`)
+console.log(...nums1)
+console.log(...'...Spread')
 
 // Spread with Arrays
+console.log('')
+console.log('Spread with Arrays')
+console.log('----------')
 
+const nums1Copy = [...nums1, 'nums1Copy'];
+const combination = [...nums1, ...nums2, 'combination'];
+
+console.log(`This is a nums1 copy using spread: ${nums1Copy}`)
+console.log(`This is a nums1 & nums2 combination using spread: ${combination}`)
 
 // Spread with Objects
+console.log('')
+console.log('Spread with Objects')
+console.log('----------')
 
+const groupOneCopy = {...groupOne};
+const bothGroups = {...groupOne, ...groupTwo}
+
+console.log(`This is a copy of groupOne`)
+console.log(groupOneCopy)
+console.log(`This is a combination of both groups`)
+console.log(bothGroups)
 
 // Rest Params
+console.log('')
+console.log('Rest Params')
+console.log('----------')
 
+function race (gold, silver, bronze, ...everyoneElse) {
+    console.log(`The Gold Medal goes to: ${gold}`)
+    console.log(`The Silver Medal goes to: ${silver}`)
+    console.log(`The Bronze Medal goes to: ${bronze}`)
+    console.log(`Thank you to everyone else for participating: ${everyoneElse}`)
+}
+race(...raceResults)
 
 // Destructing Arrays
+console.log('')
+console.log('Destructing Arrays')
+console.log('----------')
 
+const [gold, silver, bronze, ...everyoneElse] = raceResults;
+
+console.log(`Gold goes to: ${gold}`)
+console.log(`Silver goes to: ${silver}`)
+console.log(`Bronze goes to: ${bronze}`)
+console.log(` Thank you to everyone else: ${everyoneElse}`)
 
 // Destructing Objects
+console.log('')
+console.log('Destructing Objects')
+console.log('----------')
 
+const {firstName, lastName: surname, born: birthYear, died = 'N/A'} = user;
 
-// Param Destructing 
+console.log(`${firstName}, ${surname} was born ${birthYear} and death was ${died}`)
+
+// Param Destructing
+console.log('')
+console.log('Destructing Objects')
+console.log('----------')
+
+// Way One
+function fullNameOne (user) {
+    return `${user.firstName} ${user.lastName}`
+}
+
+// Way Two
+function fullNameTwo(user) {
+    const {firstName, lastName: surname} = user;
+    return `His first name was ${firstName} ${surname}`
+}
+// way Three
+function fullNameThree({firstName, lastName}) {
+    return `${firstName} ${lastName}`
+}
+
+console.log(fullNameOne(user))
+console.log(fullNameTwo(user))
+console.log(fullNameThree(user))
