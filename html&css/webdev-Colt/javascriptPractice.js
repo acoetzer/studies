8abdeae529cfd470e23342178df621cd854b9109
acoetzer <coetzeronline@gmail.com>
2022-05-08 20:40:14 +0300

    did something, cant remember what

--- html&css/webdev-Colt/javascriptPractice.js
+++
@@ -1,9 +0,0 @@
-function isTrue (arg) {
-    if (arg) {
-        console.log(true)
-        console.log('This will run')
-    } else {
-        console.log(false)
-        console.log('This wont run')
-    }
-}
