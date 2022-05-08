c5b8a15cacb606eb28074679a8d757d4b673be12
acoetzer <coetzeronline@gmail.com>
2022-05-08 09:53:16 +0300

    did something in the js practice file???

---
+++ html&css/webdev-Colt/javascriptPractice.js
@@ -0,0 +1,9 @@
+function isTrue (arg) {
+    if (arg) {
+        console.log(true)
+        console.log('This will run')
+    } else {
+        console.log(false)
+        console.log('This wont run')
+    }
+}
