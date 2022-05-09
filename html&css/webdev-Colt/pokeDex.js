503d4e6c725589074110e8012105fa319c504636
acoetzer <coetzeronline@gmail.com>
2022-05-09 14:20:53 +0300

    add pokedex and resources

---
+++ html&css/webdev-Colt/pokeDex.js
@@ -0,0 +1,15 @@
+// Where are we appending this too?
+const container = document.querySelector('.container');
+
+const url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 1; i <= 151; i++) {
+    const span = document.createElement('span');
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    span.append(h2, img);
+    h2.append(`Pokemon ${i}`);
+    img.src = `${url}${i}.png`;
+
+    container.append(span)
+}
