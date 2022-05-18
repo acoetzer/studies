d23cdcf6b03d224c4e26cd61518d522b9584550b
acoetzer <coetzeronline@gmail.com>
2022-05-18 14:45:18 +0300

    Finish Section 25 of HTML&CSS

--- html&css/webdev-Colt/script25.js
+++ html&css/webdev-Colt/script25.js
@@ -1,5 +1,27 @@
-const btn = document.querySelector('button')
+const ul = document.querySelector('#todoList');
+const form = document.querySelector('#form')
 
-window.addEventListener('click', function (e) {
-    console.log(e)
+function createEntry (username, comment) {
+    const newLi = document.createElement('li')
+    const bTag = document.createElement('b')
+
+    bTag.append(username)
+    newLi.append(bTag)
+    newLi.append(`: ${comment}`)
+    ul.append(newLi)
+
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const uName = this.elements.username
+    const uComm = this.elements.comment
+
+    createEntry(uName.value = 'defaultUser', uComm.value)
+    uName.value = ''
+    uComm.value = ''
+})
+
+ul.addEventListener('click', function (e) {
+    e.target.nodeName === 'LI' && e.target.remove()
 })
