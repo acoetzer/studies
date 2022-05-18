7a195a1b8dc075cb4b0eb2a4d7df124c9b179fc0
acoetzer <coetzeronline@gmail.com>
2022-05-18 19:40:00 +0300

    fixing typos and some mistakes

--- html&css/webdev-Colt/script25.js
+++ html&css/webdev-Colt/script25.js
@@ -1,4 +1,4 @@
-// Random Color Generator
+// RANDOM COLOR GENERATOR
 const colorBox = document.querySelectorAll('.colorBox')
 const colorText = document.querySelectorAll('.colorText')
 const gradient = document.querySelector('#gradient')
@@ -24,14 +24,13 @@ function delHistory () {
 // History function that limit the history array and removes anthing above 30.
 
 function createEntry (arr) {
-    const colorCodes = [...arr]
-
     const colorBoxHistory = document.createElement('div')
     colorBoxHistory.classList.add('colorBoxHistory')
-    colorBoxHistory.style.backgroundImage = `linear-gradient(90deg, ${colorCodes})`
+    colorBoxHistory.style.backgroundImage = `linear-gradient(90deg, ${arr[0]}, ${arr[1]}, ${arr[2]})`
     
     historyContainer.append(colorBoxHistory)
 }
+// to create a color box entry for history
 
 let colorList = []
 const history = []
@@ -40,15 +39,15 @@ const history = []
 
 for (let i = 0; i < colorBox.length; i++) {
     const color = randomColor();
-    const colorCodes = [...colorList]
+
     colorList.push(color)
     history.push(color)
     delHistory()
     
     colorBox[i].style.backgroundColor = color
     colorText[i].innerText = color
-    gradient.style.backgroundImage = `linear-gradient(90deg, ${colorCodes})`
-    btn.style.backgroundImage = `linear-gradient(90deg, ${colorCodes})`
+    gradient.style.backgroundImage = `linear-gradient(90deg, ${colorList[0]}, ${colorList[1]}, ${colorList[2]})`
+    btn.style.backgroundImage = `linear-gradient(90deg, ${colorList[0]}, ${colorList[1]}, ${colorList[2]})`
     btn.style.color = color
 }
 createEntry(colorList)
@@ -58,20 +57,37 @@ btn.addEventListener('click', function () {
     colorList = []
     for (let i = 0; i < colorBox.length; i++) {
         const color = randomColor();
-        const colorCodes = [...colorList]
         colorList.push(color)
         history.push(color)
         delHistory()
         
         colorBox[i].style.backgroundColor = color
         colorText[i].innerText = color
-        gradient.style.backgroundImage = `linear-gradient(90deg, ${colorCodes})`
-        btn.style.backgroundImage = `linear-gradient(90deg, ${colorCodes})`
+        gradient.style.backgroundImage = `linear-gradient(90deg, ${colorList[0]}, ${colorList[1]}, ${colorList[2]})`
+        btn.style.backgroundImage = `linear-gradient(90deg, ${colorList[0]}, ${colorList[1]}, ${colorList[2]})`
         btn.style.color = color
     }
     createEntry(colorList)
 })
+// Click Event to generate a random color and vaious other things.
 
 historyContainer.addEventListener('click', function (e) {
     e.target.className === 'colorBoxHistory' && e.target.remove()
 })
+// Event Delegation for removing color History boxes.
+// END OF RANDOM COLOR GENERATOR
+
+// TODO LIST
+
+
+// END OF TODO LIST
+
+// GROCERY LIST
+
+
+// END OF GROCERY LIST
+
+// LIVE UPDATE
+
+
+// END OF LIVE UPDATE
